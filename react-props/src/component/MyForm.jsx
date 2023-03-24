0b1c59ac1590be98a6dd8d23b77bd106cdaed22c
Dxchmxww <120299511+Dxchmxww@users.noreply.github.com>
2023-03-24 20:09:01 +0700

    dk

--- react-props/src/component/MyForm.jsx
+++ react-props/src/component/MyForm.jsx
@@ -39,42 +39,48 @@ function MyForm(){
 
     return(
         <>
-            <h2>Register</h2>
-            <form>
-                <label className="div">
-                    Email address
-                    <input type="text" value = {email} onChange={handleEmailChange} 
-                        required
-                        pattern ="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2, }"/>  
-                        {/* {2, } = matches two or more characters  */}
-                    
-                    <br/>
+            <div className="div">
+                <h2>Register</h2>
+                <form className="form">
+                    <label className="label">
+                        Email address:
+                        <input 
+                            type="text" 
+                            value = {email} 
+                            onChange={handleEmailChange}
+                            placeholder="Enter email" 
+                            required
+                            pattern ="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2, }"/>  
+                            {/* {2, } = matches two or more characters  */}
+                        
+                        <br/>
 
-                    Name:
-                    <input type="text" value = {username} onChange={handleUsernameChange} required/>
-                    <br/>
+                        Name:
+                        <input type="text" value = {username} onChange={handleUsernameChange} placeholder="Enter name" required/>
+                        <br/>
 
-                    Lastname:
-                    <input type="text" value = {lastname} onChange={handleLastnameChange} required/>
-                    <br/>
+                        Lastname:
+                        <input type="text" value = {lastname} onChange={handleLastnameChange} placeholder="Enter lastname" required/>
+                        <br/>
 
-                    Phone Number:
-                    <input value = {phone} onChange={handlePhoneChange} pattern="[0-9]{10}" required/>
-                    <br/>
+                        Phone Number:
+                        <input value = {phone} onChange={handlePhoneChange} pattern="[0-9]{10}" placeholder="Enter phone number" required/>
+                        <br/>
 
-                    Passsword:
-                    <input type="text" value={password} onChange={handlePasswordChange} required/>
-                    <br/>
-                    
-                </label>
+                        Passsword:
+                        <input type="text" value={password} onChange={handlePasswordChange} placeholder="Password" required/>
+                        <br/>
+                        
+                    </label>
 
-                {/* <input type="submit" value="Submit"/> */}
+                    {/* <input type="submit" value="Submit"/> */}
 
-                <button type="submit">Submit</button>
+                    <button type="submit">Submit</button>
 
 
-            </form>
+                </form>
 
+            </div>
         </>
         
         //one input
